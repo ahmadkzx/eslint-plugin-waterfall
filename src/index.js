080566fd95bf1668ac718cf6163909453b72dc9e
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-03-09 00:39:26 +0330

    feat: waterfall arguments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const WaterfallImports = require('./rules/waterfall-imports')
 const WaterfallRequires = require('./rules/waterfall-requires')
+const WaterfallArguments = require('./rules/waterfall-arguments')
 
 module.exports = {
   rules: {
     'waterfall-imports': WaterfallImports,
-    'waterfall-requires': WaterfallRequires
+    'waterfall-requires': WaterfallRequires,
+    'waterfall-arguments': WaterfallArguments,
   }
 }
