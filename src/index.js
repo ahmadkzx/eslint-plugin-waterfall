fd09a1a5a08e87eec3ffda3ac3570fae0014efa7
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-03-09 20:27:30 +0330

    refactor: remove useless waterfall arguments rule

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const WaterfallImports = require('./rules/waterfall-imports')
 const WaterfallObjects = require('./rules/waterfall-objects')
 const WaterfallRequires = require('./rules/waterfall-requires')
-const WaterfallArguments = require('./rules/waterfall-arguments')
 
 module.exports = {
   rules: {
     'waterfall-imports': WaterfallImports,
     'waterfall-objects': WaterfallObjects,
     'waterfall-requires': WaterfallRequires,
-    'waterfall-arguments': WaterfallArguments,
   }
 }
