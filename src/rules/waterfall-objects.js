df8525e29bf97a9bb3edfa92cd36912dcbb3f2f8
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-03-09 20:39:17 +0330

    feat: handle waterfall in function object arg

--- src/rules/waterfall-objects.js
+++ src/rules/waterfall-objects.js
@@ -14,9 +14,37 @@ const WaterfallObjects = {
   create(context) {
     const src = context.getSourceCode()
 
+    function isObjectPattern(node) {
+      return node.type === 'ObjectPattern'
+    }
+
+    function FunctionDeclarationHandler(node) {
+      const objectParams = node.params.filter(isObjectPattern)
+
+      objectParams.forEach(objParam => {
+        const properties = objParam.properties
+        if (!properties || properties.length === 0) return
+        const sortedProperties = [...properties].sort((nodeA, nodeB) => sortByLength(nodeA, nodeB, src))
+
+        const propertiesText = getNodesText(properties, src)
+        const sortedPropertiesText = getNodesText(sortedProperties, src)
+
+        if (sortedPropertiesText !== propertiesText) {
+          const text = sortedProperties.map(node => src.getText(node))
+          const range = [properties[0].range[0], properties[properties.length - 1].range[1]]
+          
+          context.report({
+            node,
+            message: 'Arguments should be sorted by line length',
+            fix: function(fixer) {
+              return fixer.replaceTextRange(range, text)
+            }
+          })
+        }
+      })
+    }
     return {
       'ObjectExpression:exit': function(node) {
-        console.log(node)
         const objectDeclarations = node.properties
         if (objectDeclarations.length === 0) return
         const sortedObjectDeclarations = [...objectDeclarations].sort((nodeA, nodeB) => sortByLength(nodeA, nodeB, src))
@@ -36,7 +64,11 @@ const WaterfallObjects = {
             }
           })
         }
-      }
+      },
+
+      'FunctionDeclaration:exit': FunctionDeclarationHandler,
+
+      'ArrowFunctionExpression:exit': FunctionDeclarationHandler,
     }
   }
 }
